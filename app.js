f11f23f88407246ecdfb20f1566006e077c5f5df
shmgrinsky <unrealwork@gmail.com>
2016-06-15 16:33:46 +0300

    added gzip compression

--- app.js
+++ app.js
@@ -4,8 +4,10 @@ var router = require('./routes/router');
 var express = require('express');
 var appRoot = require('app-root-path');
 var config = require('./config');
+var compression = require('compression');
 var app = express();
 
+app.use(compression());
 app.use('/api', api);
 app.use('/', router);
 app.use(express.static(appRoot + '/public'));
