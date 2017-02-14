c585b822a03da9e34b6824abe787a10d2a55a929
Unrealwork <unrealwork@gmail.com>
2017-02-14 17:44:28 +0300

    - added statistic for 2017 - added stat counter

--- config.js
+++ config.js
@@ -6,6 +6,6 @@ module.exports = {
         ttl: 3 * 60 * 60 * 24
     },
     permittedFolders: ['js', 'css', 'other'],
-    configFolders: ['energinet-2014', 'energinet-2015', 'energinet-2016'],
-    redirect: 'energinet-2016/'
+    configFolders: ['energinet-2014', 'energinet-2015', 'energinet-2016', 'energinet-2017'],
+    redirect: 'energinet-2017/'
 };
